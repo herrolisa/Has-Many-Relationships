3e1608df74ceb4cf7335e816b0eed64f3dd0de11
Lisa <misslisazhou@gmail.com>
2016-07-23 11:18:58 -1000

    create query with specific columns where comment body contains word 'SSL' or 'firewall' and post content contains word 'nemo'

--- joins.sql
+++ joins.sql
@@ -54,3 +54,14 @@ SELECT users.first_name, users.last_name, comments.body AS comment_body FROM com
   INNER JOIN users on comments.user_id = users.id
   INNER JOIN posts on comments.post_id = posts.id
   WHERE comments.body LIKE '%SSL%' AND posts.content LIKE '%dolorum%';
+
+-- 13. Create a query to get the first name of the author of the post (aliased to post_author_first_name), last name of the author of the post (aliased to post_author_last_name), the post title (aliased to post_title), username of the author of the comment (aliased to comment_author_username), and comment body (aliased to comment_body), where the comment body contains the word 'SSL' or 'firewall' and the post content contains the word 'nemo' ( should have 197 results )
+SELECT users.first_name AS post_author_first_name,
+  users.last_name AS post_author_last_name,
+  posts.title AS post_title,
+  users.username AS comment_author_username,
+  comments.body AS comment_body
+  FROM comments
+    INNER JOIN posts on comments.post_id = posts.id
+    INNER JOIN users on comments.user_id = users.id
+    WHERE (comments.body LIKE '%SSL%' OR comments.body LIKE '%firewall%') AND posts.content LIKE '%nemo%';
